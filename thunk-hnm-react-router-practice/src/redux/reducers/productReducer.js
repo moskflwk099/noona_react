1568b9bed119ea001fbb78f9dc31f0344b0c62c5
moskflwk099 <moskflwk@gmail.com>
2024-06-02 16:11:03 +0900

    thunk-hnm product

--- thunk-hnm-react-router-practice/src/redux/reducers/productReducer.js
+++ thunk-hnm-react-router-practice/src/redux/reducers/productReducer.js
@@ -2,7 +2,8 @@ let initialState={
   productList:[]
 }
 
-
+// 리듀서는 함수다. 
+// 이 리듀서의 state 값이 변경되면 store 도 업데이트 되서 화면을 다시 랜더링한다.
 function productReducer(state=initialState, action) {
   let {type, payload} = action;
   switch(type) {
@@ -11,8 +12,6 @@ function productReducer(state=initialState, action) {
       default:
         return {...state};
   }
-
-
 }
 
 export default productReducer;
